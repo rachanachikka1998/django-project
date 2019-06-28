ee67af3508e9ecd57cfa73f33b2767f891239613
rachanachikka1998 <rachanachikka1998@gmail.com>
2019-06-28 17:53:17 +0530

    changing models as per instructions

--- fbposts/models.py
+++ fbposts/models.py
@@ -5,8 +5,8 @@ import django
 # Create your models here.
 class User(models.Model):
     user_id = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50, default="")
-    pic_url = models.CharField(max_length=5000, default="")
+    username = models.CharField(max_length=50)
+    pic_url = models.CharField(max_length=5000)
 
 
 class Post(models.Model):
@@ -14,32 +14,51 @@ class Post(models.Model):
     posted_by = models.ForeignKey('User', on_delete=models.CASCADE, default=None)
     post_content = models.CharField(max_length=10000, default=None)
     posted_at = models.DateTimeField(default=None)
-    reactions = models.ForeignKey('Reaction', on_delete=models.SET_NULL, null=True)
 
 
 class Comment(models.Model):
     comment_id = models.AutoField(primary_key=True)
-    id1 = models.ForeignKey('Post',  on_delete=models.CASCADE,null=True)
-    id2 = models.ForeignKey('self',  on_delete=models.CASCADE,null=True)
-    type = models.BooleanField()
-    comment_content = models.CharField(max_length=10000, default="")
-    reactions = models.ForeignKey('Reaction', on_delete=models.CASCADE, null=True)
-    commenter_id = models.ForeignKey('User', on_delete=models.CASCADE, default=None)
+    post_field = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=True)
+    comment_field = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    comment_content = models.CharField(max_length=10000)
+    commenter_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    commented_at=models.DateTimeField(null=True)
 
-    class Meta:
-        unique_together = (("id1", "id2"),)
+
+
+    def save(self, *args, **kwargs):
+        if self.post_field == None:
+            if self.comment_field != None:
+                models.Model.save(self)
+        elif self.comment_field == None:
+            if self.post_field != None:
+                models.Model.save(self)
+        else:
+            print("check post and comment conflicts")
+
 
 
 
 class Reaction(models.Model):
     reactions=(('HAHA','HAHA'),('LIKE','LIKE'),('LOVE','LOVE'),('WOW','WOW'),('SAD','SAD'),('ANGRY','ANGRY'))
-    reaction_type=models.CharField(max_length=5,choices=reactions,default=None)
-    reactor_id=models.ForeignKey('User',on_delete=models.CASCADE,default=None)
-    reacted_to_post=models.ForeignKey('Post',on_delete=models.CASCADE,null=True)
-    reacted_to_comment=models.ForeignKey('Comment',on_delete=models.CASCADE,null=True)
-    type = models.BooleanField(default=True)
+    reaction_type = models.CharField(max_length=5,choices=reactions)
+    reactor = models.ForeignKey('User', on_delete=models.CASCADE)
+    post_field = models.ForeignKey('Post', on_delete=models.CASCADE, null=True, blank=True)
+    comment_field = models.ForeignKey('Comment', on_delete=models.CASCADE, null=True, blank=True)
+    types=(('post','post'),('comment','comment'))
+    type=models.CharField(max_length=7,choices=types)
 
+    def save(self,*args,**kwargs):
+        if self.post_field==None:
+            if self.comment_field!=None:
+                models.Model.save(self)
+        elif self.comment_field==None:
+            if self.post_field!=None:
+                models.Model.save(self)
+        else:
+            print("check post and comment conflicts")
     class Meta:
-        unique_together=(("reacted_to_post","reacted_to_comment"),)
+        unique_together=(("post_field","comment_field","reactor"),)
+
 
 
--- fbposts/views.py
+++ fbposts/views.py
@@ -11,6 +11,178 @@ import json
 import datetime
 
 
+def delete_post(post_id):
+    post=Post.objects.get(pk=post_id)
+    post.delete()
+
+def get_replies_for_comment(comment_id):
+    list_of_replies = dict()
+    comment=Comment.objects.get(pk=comment_id)
+    replies = get_comments(comment, 'comment')
+    for r in replies:
+        del r["reactions"]
+
+    return replies
+
+def get_reaction_metrics(post_id):
+    return get_count_of_each_reaction(Post.objects.get(pk=post_id), 'post')
+
+
+def get_total_reaction_count():
+    posts = Post.objects.all()
+    result = dict()
+    for post in posts:
+        dictionary = get_count_of_each_reaction(post, 'post')
+        list_of_reaction_count = dictionary.values()
+        sum = 0
+        for num in list_of_reaction_count:
+            sum += num
+        result[post.post_id] = sum
+    return result
+
+
+def get_reactions_to_post(post_id):
+    post = Post.objects.get(pk=post_id)
+    reactions = Reaction.objects.filter(reacted_to_post=post)
+    reaction_list = []
+    for reaction in reactions:
+        result = get_user_to_dict(reaction.reactor_id)
+        result["reaction"] = reaction.reaction_type
+        reaction_list.append(result)
+    return reaction_list
+
+
+def get_user_posts(user_id):
+    user = User.objects.get(pk=user_id)
+    posts = Post.objects.filter(posted_by=user)
+    list_of_posts = []
+    for post in posts:
+        list_of_posts.append(get_post(post.post_id))
+    return list_of_posts
+
+
+def get_posts_with_more_positive_reactions():
+    reactions_list = ['HAHA', 'LIKE', 'WOW', 'ANGRY', 'SAD', 'LOVE']
+    posts = Post.objects.all()
+    list_of_posts = []
+    for post in posts:
+        each_reaction_count = get_count_of_each_reaction(post, 'post')
+        positive = 0
+        negative = 0
+        for reaction in reactions_list:
+            r = each_reaction_count[reaction]
+            if r == 'SAD' or r == 'ANGRY':
+                negative += 1
+            else:
+                positive += 1
+        if positive > negative:
+            list_of_posts.append(post.post_id)
+    return list_of_posts
+
+
+def get_posts_reacted_by_user(user_id):
+    user = User.objects.get(pk=user_id)
+    reactions = Reaction.objects.filter(reactor_id=user)
+    list_of_posts = []
+    for reaction in reactions:
+        if reaction.reacted_to_post != None:
+            list_of_posts.append(get_post(reaction.reacted_to_post.post_id))
+    return list_of_posts
+
+
+def get_count_of_each_reaction(id_object, type):
+    reactions_dict = dict()
+    reactions_list = ['HAHA', 'LIKE', 'WOW', 'ANGRY', 'SAD', 'LOVE']
+    for reaction in reactions_list:
+        reactions_dict[reaction] = 0
+    try:
+        if type == True:
+            reactions = Reaction.objects.filter(reacted_to_post=id_object)
+
+        else:
+            reactions = Reaction.objects.filter(reacted_to_comment=id_object)
+    except(Exception):
+        return {"count": 0, "type": []}
+    for reaction in reactions:
+        reactions_dict[reaction.reaction_type] += 1
+    return reactions_dict
+
+
+def get_user_to_dict(user_object):
+    user = dict()
+    user["user_id"] = user_object.user_id
+    user["name"] = user_object.username
+    user["profile_pic_url"] = user_object.pic_url
+    return user
+
+
+def get_reaction_details(id_object, type):
+    reactions_dict = dict()
+    reactions_list = ['HAHA', 'LIKE', 'WOW', 'ANGRY', 'SAD', 'LOVE']
+    for reaction in reactions_list:
+        reactions_dict[reaction] = 0
+    try:
+        if type == True:
+            reactions = Reaction.objects.filter(reacted_to_post=id_object)
+
+        else:
+            reactions = Reaction.objects.filter(reacted_to_comment=id_object)
+    except(Exception):
+        return {"count": 0, "type": []}
+    for reaction in reactions:
+        reactions_dict[reaction.reaction_type] += 1
+
+    count = 0
+    final = []
+    for reaction in reactions_list:
+        count += reactions_dict[reaction]
+        if reactions_dict[reaction] != 0:
+            final.append(reaction)
+
+    reactions = dict()
+    reactions["count"] = count
+    reactions["type"] = final
+    return reactions
+
+
+def get_comments(id_object, type):
+    list_of_comments = []
+    if type == 'post':
+        comments = Comment.objects.filter(id1=id_object)
+    else:
+        comments = Comment.objects.filter(id2=id_object)
+    for comment in comments:
+        comment_dict = dict()
+        comment_dict["comment_id"] = comment.comment_id
+        comment_dict["commenter"] = get_user_to_dict(comment.commenter_id)
+        comment_dict["commented_at"] = comment.commented_at
+        comment_dict["comment_content"] = comment.comment_content
+        comment_dict["reactions"] = get_reaction_details(comment, 'commment')
+        if type == 'post':
+            replies = get_comments(comment, 'comment')
+            comment_dict["replies_count"] = len(replies)
+            comment_dict["replies"] = replies
+        list_of_comments.append(comment_dict)
+
+    return list_of_comments
+
+
+def get_post(post_id):
+    post = Post.objects.get(pk=post_id)
+    post_json = dict()
+    post_json["post_id"] = post_id
+    user = get_user_to_dict(post.posted_by)
+    post_json["posted_by"] = user
+    post_json["posted_at"] = str(post.posted_at)
+    post_json["post_content"] = post.post_content
+    reactions = get_reaction_details(post, 'post')
+    post_json["reactions"] = reactions
+    comments = get_comments(post, 'post')
+    post_json["comments"] = comments
+    post_json["comments_count"] = len(comments)
+    return post_json
+
+
 # Create your views here.
 def index(request):
     users_list = User.objects.all()
@@ -31,7 +203,7 @@ def create_post(user_id, post_content):
     try:
         post.posted_by = User.objects.get(pk=user_id)
         post.post_content = post_content
-        post.posted_at = datetime.datetime.now()
+        post.posted_at = str(datetime.datetime.now())
         post.save()
         return post.post_id
     except(Exception):
@@ -42,11 +214,12 @@ def add_comment(post_id, commenter_id, comment_text):
     comment = Comment()
 
     try:
-        comment.id1 = Post.objects.get(post_id=post_id)
-        comment.commenter_id = User.objects.get(user_id=commenter_id)
+        comment.post = Post.objects.get(pk=post_id)
+        comment.commenter_id = User.objects.get(pk=commenter_id)
         comment.comment_content = comment_text
-        comment.id2 = None
+        comment.comment = None
         comment.type = True
+        comment.commented_at = str(datetime.datetime.now())
         comment.save()
         return comment.comment_id
 
@@ -58,11 +231,11 @@ def reply_to_comment(comment_id, reply_user_id, reply_text):
     comment = Comment()
 
     try:
-        comment.id2 = Comment.objects.get(comment_id=comment_id)
-        comment.commenter_id = User.object.get(user_id=reply_user_id)
+        comment.comment = Comment.objects.get(pk=comment_id)
+        comment.commenter_id = User.objects.get(pk=reply_user_id)
         comment.comment_content = reply_text
-        comment.id1 = None
-        comment.type = False
+        comment.post = None
+        comment.commented_at = str(datetime.datetime.now())
         comment.save()
         return comment.comment_id
 
@@ -71,36 +244,49 @@ def reply_to_comment(comment_id, reply_user_id, reply_text):
 
 
 def react_to_post(user_id, post_id, reaction_type):
-    reactions_list=['HAHA','WOW','SAD','LOVE','LIKE','ANGRY']
+    reactions_list = ['HAHA', 'WOW', 'SAD', 'LOVE', 'LIKE', 'ANGRY']
     if reaction_type not in reactions_list:
         return "enter appropriate reaction"
     reaction = Reaction()
     try:
-        reaction.reactor_id = User.objects.get(user_id=user_id)
-        post = Post.objects.get(post_id=post_id)
+        user = User.objects.get(pk=user_id)
+        post = Post.objects.get(pk=post_id)
     except(Exception):
         return "enter existing user and post IDs"
-
     reactions = Reaction.objects.all()
+    flag = 0
     if len(reactions) > 0:
         for r in reactions:
-            if r.reactor_id.user_id == reaction.reactor_id.user_id:
-                if post.post_id == post_id:
+            if r.reactor_id == user:
+                if r.reacted_to_post == post_id:
                     if r.reaction_type == reaction_type:
+                        flag = 1
                         r.delete()
                         break
                     else:
-                        reaction.reacted_to_post = post
-                        reaction.reacted_to_comment = None
-                        reaction.type = True
-                        reaction.reaction_type = reaction_type
-                        reaction.save()
+                        flag = 1
+                        add_new_reaction(user, post, reaction_type, 'post')
                         r.delete()
                         break
+        if flag == 0:
+            add_new_reaction(user, post, reaction_type,'post')
     else:
-        reaction.reacted_to_post = post
-        reaction.reacted_to_comment = None
-        reaction.type = True
+        add_new_reaction(user, post, reaction_type, 'post')
+
+
+def add_new_reaction(user, id_object, reaction_type, type):
+    if type == 'post':
+        reaction = Reaction()
+        reaction.reactor = user
+        reaction.post_field = id_object
+        reaction.comment_field = None
+        reaction.reaction_type = reaction_type
+        reaction.save()
+    else:
+        reaction = Reaction()
+        reaction.reactor = user
+        reaction.post_field = None
+        reaction.comment_field = id_object
         reaction.reaction_type = reaction_type
         reaction.save()
 
@@ -111,35 +297,31 @@ def react_to_comment(user_id, comment_id, reaction_type):
         return "enter appropriate reaction"
     reaction = Reaction()
     try:
-        reaction.reactor_id = User.objects.get(user_id=user_id)
-        comment = Comment.objects.get(comment_id=comment_id)
+        user = User.objects.get(pk=user_id)
+        comment = Comment.objects.get(pk=comment_id)
     except(Exception):
         return "enter existing user and post IDs"
 
     reactions = Reaction.objects.all()
+    flag = 0
     if len(reactions) > 0:
         for r in reactions:
-            if r.reactor_id.user_id == reaction.reactor_id.user_id:
-                if comment.comment_id == comment_id:
+            if user == r.reactor_id:
+                if r.reacted_to_comment == comment:
                     if r.reaction_type == reaction_type:
+                        flag = 1
                         r.delete()
                         break
                     else:
-                        reaction.reacted_to_post = None
-                        reaction.reacted_to_comment = comment
-                        reaction.type = False
-                        reaction.reaction_type = reaction_type
-                        reaction.save()
-                        r.delete()
+                        flag = 1
+                        add_new_reaction(user, comment, reaction_type, 'comment')
                         break
+        if flag == 0:
+            add_new_reaction(user, comment, reaction_type, 'comment')
+
     else:
-        reaction.reacted_to_post = None
-        reaction.reacted_to_comment = comment
-        reaction.type = True
-        reaction.reaction_type = reaction_type
-        reaction.save()
+        add_new_reaction(user, comment, reaction_type, 'comment')
 
 
 def check():
-    b = User.objects.get(user_id=2)
-    print(b)
+    print(len({"a": 1, "b": 2}))
